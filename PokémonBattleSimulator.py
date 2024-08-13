4b6520cfa7e5dcc9e373209f67b10ebaf1dd7811
samik1999 <67670507+samik1999@users.noreply.github.com>
2024-08-14 00:26:49 +0530

    Update PokémonBattleSimulator.py

--- PokémonBattleSimulator.py
+++ PokémonBattleSimulator.py
@@ -25,32 +25,31 @@ class PokemonBattleSimulator:
     def calculate_damage(self, attacker, defender):
         attack = attacker['attack'].values[0]
         type1 = attacker['type1'].values[0]
-        type2 = attacker['type2'].values[0]
+        type2 = attacker['type2'].values[0] if attacker['type2'].values[0] else None
 
-        if f'against_{type1}' not in defender or (type2 and f'against_{type2}' not in defender):
+        if f'against_{type1}' not in defender.columns or (type2 and f'against_{type2}' not in defender.columns):
             raise ValueError("Invalid type found in the Pokémon data.")
 
         against_type1 = defender[f'against_{type1}'].values[0]
         against_type2 = defender[f'against_{type2}'].values[0] if type2 else 1.0
-
-        damage = (attack / 200) * 100 - (((against_type1 / 4) * 100) + ((against_type2 / 4) * 100))
+        damage = (((against_type1 / 4) * 100) + ((against_type2 / 4) * 100))- (attack / 200) * 100 
         return damage
 
-    def battle(self, pokemon_a_name, pokemon_b_name):
-        pokemon_a = self.find_pokemon_by_name(pokemon_a_name)
-        pokemon_b = self.find_pokemon_by_name(pokemon_b_name)
 
-        damage_a_to_b = self.calculate_damage(pokemon_a, pokemon_b)
-        damage_b_to_a = self.calculate_damage(pokemon_b, pokemon_a)
+    def battle(self, pokemon_name_a, pokemon_name_b):
+        attacker = self.find_pokemon_by_name(pokemon_name_a)
+        defender = self.find_pokemon_by_name(pokemon_name_b)
 
-        if damage_a_to_b > damage_b_to_a:
-            winner = pokemon_a_name
-            won_by_margin = damage_a_to_b - damage_b_to_a
-        elif damage_b_to_a > damage_a_to_b:
-            winner = pokemon_b_name
-            won_by_margin = damage_b_to_a - damage_a_to_b
+        damage_a = self.calculate_damage(attacker, defender)
+        damage_b = self.calculate_damage(defender, attacker)
+        print(f"{pokemon_name_a} vs {pokemon_name_b}: Damage A = {damage_a}, Damage B = {damage_b}")
+        epsilon = 0.01  # Define a small threshold for comparison
+
+        if damage_a > damage_b + epsilon:
+            return {'winner': attacker['name'].values[0]}
+        elif damage_b > damage_a + epsilon:
+            return {'winner': defender['name'].values[0]}
         else:
-            winner = "Draw"
-            won_by_margin = 0
+            return {'winner': 'Draw'}
 
-        return {"winner": winner, "won_by_margin": won_by_margin}
+        
